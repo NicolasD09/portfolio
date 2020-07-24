87d157c2e1c9a0e0ac95ade1e9a803fafb534124
Nicolas Durpoix <nicolas.durpoix82@gmail.com>
2020-07-24 11:52:03 +0200

    added main pages

---
+++ pages/experiences/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>experiences page</h1>
+</template>
+
+<script>
+export default {
+  name: 'experiences'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/projets/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>projects page</h1>
+</template>
+
+<script>
+export default {
+  name: 'projets'
+}
+</script>
+
+<style>
+
+</style>
