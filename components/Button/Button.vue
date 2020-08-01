70eb3c3fe7f7c4c967b2f7285c13d77f74189dc9
Nicolas Durpoix <nicolas.durpoix82@gmail.com>
2020-08-01 15:28:20 +0200

    init button component

---
+++ components/Button/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :class="type">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['type']
+}
+</script>
+
+<style src="./Button.css">
+</style>
